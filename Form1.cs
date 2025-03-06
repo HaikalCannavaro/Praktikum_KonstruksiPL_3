f1eb371e8685cc1497cfd419d21740bbbf80d856
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-06 10:24:47 +0700

    Menambahkan layout GUI kalkulator

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,52 @@ namespace modul3_103022300106
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
